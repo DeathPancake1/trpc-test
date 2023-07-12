1f9696a7e29a625c6afeb924b266a5fcd0d67515
Youssef Dawoud <youssefyoyododo@gmail.com>
2023-07-12 13:33:15 +0300

    Added link styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Home() {
+  const linkStyle = {
+    margin: "30px",
+    padding: "3px",
+    border: "1px solid black",
+    borderRadius: "5px"
+  };
     return (
       <div className="App">
-        <a href="/register">Register</a>
-        <a href="/login">Login</a>
+        <Link to={"./register"} style={linkStyle}>Register</Link>
+        <Link to={"./login"} style={linkStyle}>Login</Link>
       </div>
     );
   }
