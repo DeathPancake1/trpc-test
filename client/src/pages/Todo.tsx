2094d375fafc75f295c726b2937f2fdcf06d331d
Youssef Dawoud <youssefyoyododo@gmail.com>
2023-07-17 13:37:34 +0300

    Added Dockerization for backend and database

--- client/src/pages/Todo.tsx
+++ client/src/pages/Todo.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { TodoType } from "../../../server/types"
+import { TodoType } from "../types"
 import trpc from "../trpcClient";
 
 function Todo() {
